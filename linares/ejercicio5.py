b51dc93df520d09d4afe245f4fe512fc9480a642
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-12-19 12:16:02 -0500

    ejercicio cinco terminado

---
+++ linares/ejercicio5.py
@@ -0,0 +1,7 @@
+import os
+import libreria
+m=int(os.sys.argv[1])
+n=int(os.sys.argv[2])
+p=int(os.sys.argv[3])
+volumen=libreria.volumen_rectoedro(m,n,p)
+print("El volumen del rectoedro es:", volumen)

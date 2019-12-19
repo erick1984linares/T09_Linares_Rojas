7910ab75e237e1cd9db1bbe7f79bfb31447be071
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-12-19 12:16:16 -0500

    ejercicio seis terminado

---
+++ linares/ejercicio6.py
@@ -0,0 +1,7 @@
+import os
+import libreria
+x=int(os.sys.argv[1])
+y=int(os.sys.argv[2])
+z=int(os.sys.argv[3])
+area=libreria.area_lateral_de_un_cono(x,y,z)
+print("El area lateral del cono es:", area)

06a72ab0ef0f1187d9a65377c5772c667feb96e5
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-12-19 12:18:26 -0500

    ejercicio trece terminado

---
+++ linares/ejercicio13.py
@@ -0,0 +1,7 @@
+import os
+import libreria
+q=int(os.sys.argv[1])
+w=int(os.sys.argv[2])
+
+aceleracion_angular1=libreria.aceleracion_angular(q,w)
+print("La aceleracion angular es:", aceleracion_angular1)

443ebe5b072efd7f78ef2a0fff2f0aa2ec613c29
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-12-19 12:15:10 -0500

    ejercicio tres terminado

---
+++ linares/ejercicio3.py
@@ -0,0 +1,7 @@
+import os
+import libreria
+a=int(os.sys.argv[1])
+b=int(os.sys.argv[2])
+
+area_lateral=libreria.area_lateral_de_un_prisma(a,b)
+print("Elarea lateral del prismas es:", area_lateral)

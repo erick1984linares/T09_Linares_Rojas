717d46bfd460366ef53701a9f0ff5a9f1646f743
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-12-19 12:17:51 -0500

    ejercicio once terminado

---
+++ linares/ejercicio11.py
@@ -0,0 +1,7 @@
+import os
+import libreria
+a=int(os.sys.argv[1])
+b=int(os.sys.argv[2])
+
+variacion=libreria.variacion_de_energia_interna(a,b)
+print("La variacion de energia interna es:", variacion)
